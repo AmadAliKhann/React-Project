017fa1cbcf0b1ff762421ad40b2a30131436fd61
AmadAliKhann <amadalikhan@live.com>
2023-03-08 12:26:18 +0100

    deployment check

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import {Suspense} from 'react'
+import { Suspense } from 'react'
 import Meal from "./Components/Meal";
 import './App.css';
-import {Routes,Route} from "react-router-dom"
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import RecipeInfo from './Components/RecipeInfo';
+
 function App() {
-  
   return (
     <>
-    <Suspense fallback = {null}>
-    <Routes>
-      <Route path='/' element ={<Meal/>}/>
-      <Route path="/:MealId" element={<RecipeInfo/>}/>
-    </Routes>
-    </Suspense>
-    
-
+      <HashRouter basename="/">
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path='/' element={<Meal />} />
+            <Route path="/:MealId" element={<RecipeInfo />} />
+          </Routes>
+        </Suspense>
+      </HashRouter>
     </>
   )
-  }
+}
+
 export default App;
