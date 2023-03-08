31ad28c6e1c4484f9218f88cbe8f4527e6e3201a
AmadAliKhann <amadalikhan@live.com>
2023-03-08 12:56:10 +0100

    deployment try 1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import {Suspense} from 'react'
 import Meal from "./Components/Meal";
 import './App.css';
-import {Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import RecipeInfo from './Components/RecipeInfo';
 function App() {
   
   return (
     <>
+      <BrowserRouter basename='/React-Project'>
+
     <Suspense fallback = {null}>
     <Routes>
       <Route path='/' element ={<Meal/>}/>
       <Route path="/:MealId" element={<RecipeInfo/>}/>
     </Routes>
     </Suspense>
+    </BrowserRouter>
     
 
     </>
